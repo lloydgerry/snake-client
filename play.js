a38451db71373f64515070eda93a6d2c5b1ccfdb
Lloyd <lloyd.gerry@gmail.com>
2019-09-25 20:42:33 +0000

    updating pplay and create client

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.43.245',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
